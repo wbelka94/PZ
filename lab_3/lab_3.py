3cd785ccee96e270ddb3c8c7db62d00dcbd36ba5
wbelka <wbelka94@gmail.com>
2017-03-21 16:46:54 +0100

    tf_idf

--- lab_3/lab_3.py
+++ lab_3/lab_3.py
@@ -3,6 +3,7 @@ from sklearn import svm
 from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords, reuters
 import re
+import math
 
 cahcedStopWords = stopwords.words("english")
 min_length = 3
@@ -116,6 +117,31 @@ class document:
         for word in self.text.split():
             vector[inverse_vocabulary[word]] = 1
         return vector
+
+class tf_idf:
+    def __init__(self):
+        self.D = 0.0
+        self.df = {}
+
+    def add_document(self, document):
+        self.D += 1.0
+        for token in set(document):     #set() tworzy liste unikalnych tokenow
+            self.df[token] += 1.0
+
+    def idf(self, token):
+        return math.log(self.D/self.df[token])
+
+    def tf(self, token, document):
+        liczba_wystapien_tokenu = 0.0
+        liczba_tokenow = 0.0
+        for t in document:
+            liczba_tokenow += 1.0
+            if t == token:
+                liczba_wystapien_tokenu += 1.0
+        return liczba_wystapien_tokenu/liczba_tokenow
+
+    def tf_idf(self, token, document):
+        return self.tf(token,document) * self.idf(token)
 
 dir_neg = "C:\\Users\\s0152850\\Desktop\\txt_sentoken\\neg"
 dir_pos = "C:\\Users\\s0152850\\Desktop\\txt_sentoken\\pos"
